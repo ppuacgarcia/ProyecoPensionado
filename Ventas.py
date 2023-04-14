b85feb8572641f7a2f57e182b271cc8a53937b4f
Jose Daniel Az <josedanielxela@gmail.com>
2023-04-13 23:18:49 -0600

    Update Ventas.py

--- Ventas.py
+++ Ventas.py
@@ -6,26 +6,93 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvas
+
+
+hiColor='#65A0A3'
+
 class Ventas:
     def __init__(self,ventanaPrincipal):
-        table = ttk.Treeview(ventanaPrincipal)
-        table['columns'] = ('player_id', 'player_name', 'player_Rank', 'player_states', 'player_city')
-        table.column("#0", width=0,  stretch=NO)
-        table.column("player_id",anchor=CENTER, width=80)
-        table.column("player_name",anchor=CENTER,width=80)
-        table.column("player_Rank",anchor=CENTER,width=80)
-        table.column("player_states",anchor=CENTER,width=80)
-        table.column("player_city",anchor=CENTER,width=80)
+        self.w = Frame(ventanaPrincipal,width=1200,height=675,bg='#3F5657')
+        self.w.place(x=0, y=0)
+        self.fuenteG = font=('Arial', 19,'bold')
+        self.fuenteP = font=('Arial', 15,'bold')
+        self.bglabel = '#3F5657'
+        self.fglabel = '#FFFFFF'
+        self.posx = 80
 
-        table.heading("#0",text="",anchor=CENTER)
-        table.heading("player_id",text="Id",anchor=CENTER)
-        table.heading("player_name",text="Name",anchor=CENTER)
-        table.heading("player_Rank",text="Rank",anchor=CENTER)
-        table.heading("player_states",text="States",anchor=CENTER)
-        table.heading("player_city",text="States",anchor=CENTER)
-        table.pack()
+        #Etiquetas
+        self.lab('Ventas', self.fuenteG, self.bglabel, self.fglabel, 580, 10)
+        self.lab('Nombre Comida', self.fuenteP, self.bglabel, self.fglabel, self.posx, 55)
+        self.lab('Proteina', self.fuenteP, self.bglabel, self.fglabel, self.posx, 85)
+
+        #Cuadros de texto
+        self.nombreComida=self.Ent(65, 250, 60)
         
+        
+        #comboBox
+        self.combProteina = ['Res','Pollo','Mariscos', 'Cerdo']
+        self.comboRan = ttk.Combobox(self.w, value=self.combProteina, width=62)
+        self.comboRan.place(x=250, y=90)
+        self.comboRan["state"]="readonly"
+        self.comboRan.current(1)
+        
+        
+        #Botones
+        self.Guardar = self.btn(975, 600, 'guardar', '#FFFFFF', hiColor, self.on_button_click, 'Arial', 12,'bold',18,2)
 
+               
+        
+    def cmd(self):
+        self.conn.commit()
+        self.w.destroy()
 
-       
-      
+    #Labels formulario
+    def lab(self,text, font, bg, fg, x, y):
+        labe = Label(self.w,text=text, font=font, bg=bg, foreground=fg)
+        labe.pack()
+        labe.place(x=x, y=y)
+    
+    #Cuadros de Texto
+    def Ent(self, width, x, y):
+        Entr = Entry(self.w,width=width)
+        Entr.pack()
+        Entr.place(x=x, y=y)
+        return Entr
+
+    #Funcion para crear botones
+    def btn(self, x, y, text, bcolor, fcolor, command, font, siz, tipe,wdt,ht):
+        #Botones para menu
+        def on_enter(e):
+            buttons['background'] = bcolor
+            buttons['foreground'] = fcolor
+            
+        def on_leave(e):
+            buttons['background'] = fcolor
+            buttons['foreground'] = bcolor
+            
+            
+        buttons = Button(self.w, width=wdt, height=ht, text= text, fg  = bcolor, bg=fcolor, command=command, border=0, activebackground=bcolor, activeforeground=fcolor,font=(font, siz, tipe))
+        buttons.bind("<Enter>", on_enter)
+        buttons.bind("<Leave>", on_leave)
+        buttons.place(x=x, y=y)
+        return buttons
+    
+    def tb(self,x,y):
+        tabla=Listbox(self.w)
+        tabla.place(x=x,y=y)
+        tabla.config(height=1)
+        return tabla
+    
+    
+
+    def mayus(self,nombreB):
+        result=""
+        for i in range ( len (nombreB) ):
+            if(ord(nombreB[i])>96 and ord(nombreB[i])<122):
+               result+=chr(ord(nombreB[i])-32)
+            else:
+                result+=nombreB[i]
+        return result
+    
+    def on_button_click(self):
+        pass
