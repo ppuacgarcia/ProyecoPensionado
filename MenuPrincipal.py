e7e94d6fb61b4817779d4f60270b690f190b4959
Jose Daniel Az <josedanielxela@gmail.com>
2023-04-25 17:25:00 -0600

    Conexion y btn menu

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -21,7 +21,7 @@ class MenP:
         self.btnmenu(self.fMenuP, 400, 276, 'Hospedaje', hiColor,bgcolor, self.cmdPensionistas,'Arial', 16,'bold',)
     def cmdVentas(self):
        Ventas(self.fMenuP)
-       self.pw.destroy() 
+       #self.pw.destroy() 
     #comando para entrar a Hospedaje
     def cmdHospedaje(self):
        Pensionistas(self.fMenuP)
